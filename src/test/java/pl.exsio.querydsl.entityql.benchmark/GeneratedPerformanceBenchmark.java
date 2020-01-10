83a0e4c291851ae9efca5b854b0dce39273a7e43
eXsio <sdymitrow@gmail.com>
2020-01-10 12:44:28 +0100

    2.0 - static meta model generation

---
+++ src/test/java/pl.exsio.querydsl.entityql.benchmark/GeneratedPerformanceBenchmark.java
@@ -0,0 +1,29 @@
+package pl.exsio.querydsl.entityql.benchmark;
+
+import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.RunnerException;
+import pl.exsio.querydsl.entityql.config.entity.it.OrderItem;
+import pl.exsio.querydsl.entityql.config.entity.it.generated.QOrderItem;
+
+import static org.junit.Assert.assertTrue;
+import static pl.exsio.querydsl.entityql.EntityQL.qEntity;
+
+public class GeneratedPerformanceBenchmark implements PerformanceBenchmark {
+
+    @Benchmark
+    @Fork(value = 1)
+    @Warmup(iterations = 6, time = 10)
+    @Measurement(iterations = 6, time = 10)
+    @BenchmarkMode(Mode.Throughput)
+    public void run(Blackhole blackHole) {
+        blackHole.consume(new QOrderItem());
+    }
+
+    @Test
+    public void shouldRunGeneratedMetaModelCreationBenchmark() throws RunnerException {
+        double score = runBenchmark();
+        assertTrue(score >= 0);
+    }
+}
---
+++ src/test/java/pl.exsio.querydsl.entityql.benchmark/PerformanceBenchmark.java
@@ -0,0 +1,22 @@
+package pl.exsio.querydsl.entityql.benchmark;
+
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface PerformanceBenchmark {
+
+    default double runBenchmark() throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(getClass().getSimpleName())
+                .build();
+        List<RunResult> runResults = new ArrayList<>(new Runner(opt).run());
+        assert !runResults.isEmpty();
+        return runResults.get(0).getPrimaryResult().getScore();
+    }
+}
