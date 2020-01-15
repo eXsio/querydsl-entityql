b13caf79e5963ce0d824e1bfc1f86a0bba6e12f0
lpandzic <lovro.pandzic@infobip.com>
2020-01-15 13:26:43 +0100

    added spring data jdbc scanner

---
+++ src/test/groovy/pl/exsio/querydsl/entityql/spec/QExporterSpringDataJdbcITSpec.groovy
@@ -0,0 +1,39 @@
+package pl.exsio.querydsl.entityql.spec
+
+
+import pl.exsio.querydsl.entityql.QExporter
+import pl.exsio.querydsl.entityql.entity.scanner.QEntityScanner
+import pl.exsio.querydsl.entityql.entity.scanner.SpringDataJdbcQEntityScanner
+import pl.exsio.querydsl.entityql.jdbc.entity.UpperCaseWithUnderscoresNamingStrategy
+import pl.exsio.querydsl.entityql.jdbc.entity.it.*
+import spock.lang.Ignore
+import spock.lang.Specification
+
+import static pl.exsio.querydsl.entityql.EntityQL.qEntity
+
+public class QExporterSpringDataJdbcITSpec extends Specification {
+
+    String pkgName = "pl.exsio.querydsl.entityql.jdbc.entity.it.generated"
+
+    String fileNamePattern = "Q%s.groovy"
+
+    String destinationPath = java.nio.file.Paths.get("src/test/groovy").toAbsolutePath()
+
+    QExporter exporter = new QExporter();
+    QEntityScanner scanner = new SpringDataJdbcQEntityScanner(new UpperCaseWithUnderscoresNamingStrategy());
+
+//    @Ignore
+    def "should export integration q classes to static meta models"() {
+        when:
+        exporter.export(qEntity(Book, scanner), fileNamePattern, pkgName, destinationPath)
+        exporter.export(qEntity(Group, scanner), fileNamePattern, pkgName, destinationPath)
+        exporter.export(qEntity(GroupAdmin, scanner), fileNamePattern, pkgName, destinationPath)
+        exporter.export(qEntity(Order, scanner), fileNamePattern, pkgName, destinationPath)
+        exporter.export(qEntity(OrderItem, scanner), fileNamePattern, pkgName, destinationPath)
+        exporter.export(qEntity(UploadedFile, scanner), fileNamePattern, pkgName, destinationPath)
+        exporter.export(qEntity(User, scanner), fileNamePattern, pkgName, destinationPath)
+
+        then:
+        noExceptionThrown()
+    }
+}
